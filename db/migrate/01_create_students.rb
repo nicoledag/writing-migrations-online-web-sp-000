d2a13d16efad7eecf73fc76acc0d96fcc63b9f73
Nicole D <nnardella@gmail.com>
2019-01-08 18:48:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
 
-  sql = <-SQL
-    CREATE TABLE IF NOT EXISTS artists (
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
     )
